94576cb3165c33541ac91ae017612ecde12ae544
odunuzz <33123214+odunuzz@users.noreply.github.com>
2020-06-14 16:51:37 +0300

    Add files via upload

---
+++ 11.05.2018/11.05.2018/Idurum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._05._2018
+{
+    interface Idurum
+    {
+        void paraYukle();
+        void paraIade();
+        void urunVer();
+        void durumGoster();
+    }
+}
